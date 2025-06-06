2a0187dfac1c3752bd181a97b7ca1f7359fdc6c2
dsosnovskiy <dsosnovskiy2006@gmail.com>
2025-06-06 23:06:26 +0300

    added: new font "Exo2_Regular"

---
+++ app/src/main/java/com/example/weatherappcompose/ui/theme/Font.kt
@@ -0,0 +1,10 @@
+package com.example.weatherappcompose.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.weatherappcompose.R
+
+val exo2 = FontFamily(
+    Font(R.font.exo2_regular, FontWeight.Normal),
+)
